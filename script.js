69e722fc787a08595629bb9cda9c8bea64b70994
Filipe Robert <filipe_soares@yahoo.com>
2024-05-29 12:53:07 -0300

    Previs-o-do-tempo commit

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-const search = document.getElementById('btn')
+const search = document.getElementById('btn');
+const cityLocation = document.querySelector('#city-location');
+const temp = document.querySelector('#temp');
+const maxGraus = document.querySelector('#Maxgraus');
+const minGraus = document.querySelector('#Mingraus');
+const moisture = document.querySelector('#umidade');
+const imgeTemp = document.querySelector('#image-temp');
 
 const key = "f23a5b4b33ffc27cfd5de780fce37d7a" //chave da API
-
-search.addEventListener('click', clickBtn); 
-
-function screanDados(dados){
-    console.log(dados)
-    document.querySelector('#city-location').innerHTML = "Tempo em " + dados.name
-    document.querySelector('#temp').innerHTML = dados.weather[0].description
-    document.querySelector('#Maxgraus').innerHTML = "Max: " + Math.floor(dados.main.temp_max) + " C°" //arredondar temp
-    document.querySelector('#Mingraus').innerHTML = "Min: " + Math.floor(dados.main.temp_min) + " C°"
-    document.querySelector('#umidade').innerHTML = "umidade: " +  dados.main.humidity + "%"
-    document.querySelector('#image-temp').src=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
-    
-}
 
 async function searchCity(city){ //Async----funçao assincrona//
 
@@ -21,11 +14,32 @@ async function searchCity(city){ //Async----funçao assincrona//
     screanDados(dados)
 }
 
+search.addEventListener('click', clickBtn); 
+
+function screanDados(dados){
+    cityLocation.innerHTML = "Tempo em " + dados.name
+    temp.innerHTML = dados.weather[0].description
+    maxGraus.innerHTML = "Max: " + Math.floor(dados.main.temp_max) + " C°" //arredondar temp
+    minGraus.innerHTML = "Min: " + Math.floor(dados.main.temp_min) + " C°"
+    moisture.innerHTML = "umidade: " +  dados.main.humidity + "%"
+    imgeTemp.src=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    
+}
+
+
 function clickBtn(){
     //pegando valor input do html//
     const city = document.querySelector("#input-city").value;
+    if(!city){
+        cityLocation.innerHTML = "Não Encontrado";
+    }else{
+        searchCity(city);
+    }
     
-
-    searchCity(city)
    
 }
+document.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        clickBtn();
+    }
+})
